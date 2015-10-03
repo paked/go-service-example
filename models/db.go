c14ec4095c62f8522f03609989a68d6c458bafe5
Harrison <harrison@theshoebridges.com>
2015-10-03 23:12:54 +1000

    Add basic models

---
+++ models/db.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"gopkg.in/mgutz/dat.v1/sqlx-runner"
+)
+
+var DB *runner.DB
+
+func Init(user, pass, service, port, name string) {
+	db, err := sql.Open("postgres",
+		fmt.Sprintf("postgresql://%v:%v@%v:%v/%v?sslmode=disable",
+			user,
+			pass,
+			service,
+			port,
+			name,
+		),
+	)
+
+	if err != nil {
+		fmt.Println("lol")
+		panic(err)
+	}
+
+	runner.MustPing(db)
+
+	runner.LogQueriesThreshold = 10 * time.Millisecond
+	DB = runner.NewDB(db, "postgres")
+
+	DB.DB.MustExec(query)
+}
+
+var query = `
+CREATE TABLE IF NOT EXISTS pings (
+	id serial PRIMARY KEY,
+	message text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+	id serial PRIMARY KEY,
+	username text NOT NULL,
+	password text NOT NULL,
+	email text NOT NULL
+);
+`
---
+++ models/user.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+)
+
+type User struct {
+	ID       int64  `db:"id"`
+	Username string `db:"username"`
+	Password string `db:"password"`
+	Email    string `db:"email"`
+}
+
+func NewUser(username, password, email string) (User, error) {
+	var u User
+
+	err := DB.
+		Select("*").
+		From("users").
+		Where("username = $1", username).
+		QueryStruct(&u)
+
+	if err == nil || u != (User{}) {
+		return u, errors.New("That user already exists")
+	}
+
+	err = DB.
+		InsertInto("users").
+		Columns("username", "password", "email").
+		Values(username, password, email).
+		Returning("*").
+		QueryStruct(&u)
+
+	if err != nil {
+		return u, err
+	}
+
+	if u.Username != username {
+		fmt.Println(u)
+		return u, errors.New("User did not sync with database")
+	}
+
+	return u, err
+}
